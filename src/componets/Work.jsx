2c01811c609f2867afbaafdfbf23db3d96e7f334
favourflav <chukwagon1@gmail.com>
2023-04-13 10:36:39 -0700

    added new project

--- src/componets/Work.jsx
+++ src/componets/Work.jsx
@@ -7,6 +7,7 @@ import memories from '../assets/memories.png'
 import freeCode from '../assets/freecode.png'
 import commerce from '../assets/commerce.png'
 import college from '../assets/college.png'
+import chat from '../assets/chat.png'
 
 export default function Work() {
   return (
@@ -33,9 +34,9 @@ export default function Work() {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-base font-bold text-white tracking-wider">
+              <div className="text-sm font-bold text-white tracking-wider p-2">
                 E-Commerce Full Stack React JS Application, this app is deployed using Render. I used redux toolkit to handle my state values, and also used redux toolkit persist to persist my state. This app also has strapi and stripe configurations.
-              </span>
+              </div>
               <div className="pt-8 text-center">
                 <a
                   href="https://ecommerce-app-4n5y.onrender.com/"
@@ -69,6 +70,54 @@ export default function Work() {
             </div>
           </div>
 
+
+          {/* Indivdual Card item/box */}
+          <div
+            style={{ backgroundImage: `url(${chat})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div className="text-sm font-bold text-white tracking-wider p-2">
+              This was a complicated app for me but I learned a lot. Watched a youtube video mainly on the backend implementation. Focusing on the mongodb relations within modal. Each chat group and message modal have relations with each other in order making it easy to populate them and send them back to the frontend as a whole singular object.
+
+              <br />
+
+              A user will be able to login and or sign up and be able to send messages to various users using socket io and mongo db
+              </div>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://message-frontend.onrender.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center  rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/favourflav2/Message-Backend"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center  rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                     Backend Code
+                  </button>
+                </a>
+
+                <a
+                  href="https://github.com/favourflav2/Message-Frontend"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center  rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Frontend Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
           {/* Indivdual Card item/box */}
           <div
             style={{ backgroundImage: `url(${college})` }}
@@ -76,9 +125,9 @@ export default function Work() {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className=" text-sm font-bold text-white tracking-wider ">
+              <div className=" text-sm font-bold text-white tracking-wider p-2">
               This app I incorporated the College Board api, with this a user can search a college and retrieve valuable data. If a user is logged in and authenticated they can save colleges and view them in their dashboard.
-              </span>
+              </div>
               <div className="pt-8 text-center">
                 <a
                   href="https://college-app-frontend.onrender.com"
@@ -121,9 +170,9 @@ export default function Work() {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className=" text-sm font-bold text-white tracking-wider ">
+              <div className=" text-sm font-bold text-white tracking-wider p-2">
                 This is an anime app I deployed using render for my backend and netlify for my frontend. I used a lot of concepts from other projects I created from youtube. Using Mongdb to store users and authenticating users with jwt. Logged in users will be able to save animes to their dashboard pages.
-              </span>
+              </div>
               <div className="pt-8 text-center">
                 <a
                   href="https://anime-app.onrender.com"
@@ -167,11 +216,11 @@ export default function Work() {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className=" text-sm font-bold text-white tracking-wider">
+              <div className=" text-sm font-bold text-white tracking-wider p-2">
               This is a MERN stack app that utilizes Nodejs and Express for the backend and React for the front end. A user that is logged in will be authenticated using jsonwebtokens and then be able to like, edit, and delete posts. This a pretty hefty app, it takes a little to load. The like functionality pushes a logged in users ID into the post users likes array. Clicking the same like button will filter the likes array and delete the ID, unliking the post. You can then go to your dashboard page where you can edit or delete your posts.
 
  
-              </span>
+              </div>
               <div className="pt-8 text-center">
                 <a
                   href="https://memories-mern-app-favour.netlify.app/"
